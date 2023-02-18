fd9e30edc3bb7b53d3eb2bdb459ba2a16f8ffa4a
zzee86 <94978880+zzee86@users.noreply.github.com>
2023-02-18 16:00:00 +0000

    Player Movement

---
+++ Invaders/Assets/Scripts/menu/Navigator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Navigator : MonoBehaviour
+{
+public void SinglePlayerBtn(){
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("SinglePlayer");
+
+}
+}
