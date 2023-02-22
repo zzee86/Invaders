b39eb37ab001728190d1ef8541a979b0ee12f29a
zzee86 <94978880+zzee86@users.noreply.github.com>
2023-02-22 23:33:04 +0000

    Loading Menu (may not use)

---
+++ Invaders/Assets/_Scripts/Manager/LevelManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private Slider slider;
+
+
+
+    public void LoadLevel(string sceneName)
+    {
+        StartCoroutine(LoadAsynchronously(sceneName));
+    }
+    IEnumerator LoadAsynchronously(string sceneName)
+    {
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            yield return null;
+
+        }
+
+    }
+}
+
+
+
+/*
+    public static LevelManager instance;
+    [SerializeField] private GameObject loadingScene;
+    [SerializeField] private Slider slider;
+    private float _target;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public async void LoadScene(string sceneName)
+    {
+        _target = 0;
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+        loadingScene.SetActive(true);
+
+        do
+        {
+            _target = scene.progress;
+        }
+        while (scene.progress < 0.9f);
+        scene.allowSceneActivation = true;
+        loadingScene.SetActive(false);
+    }
+
+    void Update()
+    {
+        slider.value = Mathf.MoveTowards(slider.maxValue, _target, 3 * Time.deltaTime);
+        Debug.Log(slider.value);
+        WaitForSeconds.Equals("20");
+    }
+}
+*/
