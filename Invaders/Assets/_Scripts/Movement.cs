9f3079a14a9281ed6cbcbe861b2c4404a36fbb83
zzee86 <94978880+zzee86@users.noreply.github.com>
2023-02-27 13:00:17 +0000

    Wall Slide And Jump

--- Invaders/Assets/_Scripts/Movement.cs
+++ Invaders/Assets/_Scripts/Movement.cs
@@ -16,7 +16,46 @@ public class Movement : MonoBehaviour
 
     bool facingRight = true;
 
+
+
+
+    private bool isWallSliding;
+    private float wallSlideSpeed = 0.1f;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+
+
+
+
+    // Jumping from walls
+    private bool isWallJump;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+
+
     // Start is called before the first frame update
+
+    private bool isWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+    private void wallSlide()
+    {
+        if (isWalled() && !grounded)
+        {
+            isWallSliding = true;
+            body.velocity = new Vector2(body.velocity.x, Mathf.Clamp(body.velocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+
+    }
+
 
 
     private void Awake()
@@ -56,6 +95,17 @@ public class Movement : MonoBehaviour
         anim.SetBool("Grounded", grounded);
 
 
+        wallSlide();
+        wallJump();
+
+        // if (!isWallJump)
+        // {
+        //     flip();
+        // }
+
+
+
+
         /*
                 Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -69,6 +119,7 @@ public class Movement : MonoBehaviour
                     flip();
                 }
         */
+
     }
     void flip()
     {
@@ -76,13 +127,44 @@ public class Movement : MonoBehaviour
         transform.Rotate(0f, 180f, 0f);
     }
 
-
     private void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, jumpPower);
         anim.SetTrigger("Jump");
         jumpCount -= 1;
         grounded = false;
+    }
+    private void wallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJump = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+            CancelInvoke(nameof(StopWallJumping));
+
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            isWallJump = true;
+            body.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDirection)
+            {
+              flip();
+            }
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+    private void StopWallJumping()
+    {
+        isWallJump = false;
     }
 
     //When player collides with Ground, reset number of jumps
@@ -93,5 +175,7 @@ public class Movement : MonoBehaviour
             grounded = true;
         }
         jumpCount = maxJumps;
+
     }
+
 }
