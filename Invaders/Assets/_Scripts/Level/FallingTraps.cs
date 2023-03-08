744dacd4fb9f111ed7733bc56d5cb227e15bf5c4
zzee86 <94978880+zzee86@users.noreply.github.com>
2023-03-08 19:58:12 +0000

    Falling Spikes

---
+++ Invaders/Assets/_Scripts/Level/FallingTraps.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTraps : MonoBehaviour
+{
+    Rigidbody2D body;
+    [SerializeField] private LayerMask playerLayer;
+    private RaycastHit2D playerHit;
+    [SerializeField] private float destroyTimer = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectPlayer();
+    }
+    void DetectPlayer()
+    {
+        playerHit = Physics2D.Raycast(transform.position, Vector2.down, 100f, playerLayer);
+
+        Debug.DrawRay(transform.position, Vector2.down * 100f, Color.red);
+        if (playerHit)
+        {
+            body.gravityScale = 1f;
+            Debug.Log("Falling");
+            Destroy(gameObject, destroyTimer);
+        }
+    }
+}
