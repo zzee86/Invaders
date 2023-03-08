5f74a6511de600d622309d8e47e3fa26da42e20d
zzee86 <94978880+zzee86@users.noreply.github.com>
2023-03-08 21:51:25 +0000

    Destroy Falling Block

--- Invaders/Assets/_Extended/Scripts/FallingBlock.cs
+++ Invaders/Assets/_Extended/Scripts/FallingBlock.cs
@@ -4,11 +4,15 @@ public class FallingBlock : MonoBehaviour
 {
     [SerializeField] private GameObject dustParticles;
     [SerializeField] private Vector3 Offset;
+    [SerializeField] private GameObject shadow;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
 
         AudioManager.PlayRockCrunch();
         Instantiate(dustParticles, transform.position + Offset, Quaternion.identity);
+        Destroy(gameObject, 2);
+        Destroy(shadow, 2);
+
     }
 }
